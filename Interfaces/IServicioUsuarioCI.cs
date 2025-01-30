5324bb87e8bc54616dee25d56e6fbec8db1243d4
Edilson Rafael Alcantara Ureña <edilsona@bellon.com.do>
2025-01-30 11:50:50 -0400

    agregue nuevos cambios en la interfaz de usuarios ci

--- Interfaces/IServicioUsuarioCI.cs
+++ Interfaces/IServicioUsuarioCI.cs
@@ -6,6 +6,8 @@ public interface IServicioUsuarioCI
 {
     Task<List<Usuario>> ObtenerUsuarios();
     Task<Usuario> ObtenerUsuario(int? id);
+    Task<Usuario> ObtenerUsuarioPorCorreo(string? correo);
+    Task<List<Usuario>> ObtenerUsuarioResponsablesPorDepartamentos(string? departamentoId);
     Task<Usuario> GuardarUsuario(Usuario usuario);
     Task<Usuario> EliminarUsuario(int id);
     Task<bool> RefrescarCache();
