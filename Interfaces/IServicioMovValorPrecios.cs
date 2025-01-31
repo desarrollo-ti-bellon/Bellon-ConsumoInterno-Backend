2d6c6eb6eefae95fb7322da4af57df05efe4d08a
Edilson Rafael Alcantara Ureña <edilsona@bellon.com.do>
2025-01-31 11:03:32 -0400

    limpiando la solucion completa

--- Interfaces/IServicioMovValorPrecios.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bellon.API.Liquidacion.Interfaces
-{
-    public interface IServicioMovValorPrecios
-    {
-        Task<List<Classes.LSCentralMovValorPrecios>> ObtenerMovValorPrecios(string no_documento );
-    }
-}
