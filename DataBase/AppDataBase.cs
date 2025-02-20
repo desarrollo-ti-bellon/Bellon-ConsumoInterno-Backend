961ffe99b0847cf9804d4322b949a57125e0217f
Edilson Alcantara <edilsona@bellon.com.do>
2025-02-20 08:37:01 -0400

    reparando el contexto de la base de datos

--- DataBase/AppDataBase.cs
+++ DataBase/AppDataBase.cs
@@ -6,20 +6,19 @@ namespace Bellon.API.ConsumoInterno.DataBase;
 
 public partial class AppDataBase : DbContext
 {
-
     public virtual DbSet<Aplicacion> Aplicacion { get; set; }
+
+    public virtual DbSet<CabeceraConsumosInternos> CabeceraConsumosInternos { get; set; }
 
     public virtual DbSet<CabeceraSolicitudesCI> CabeceraSolicitudesCI { get; set; }
 
     public virtual DbSet<ClasificacionesCI> ClasificacionesCI { get; set; }
 
-    public virtual DbSet<CabeceraConsumoInterno> CabeceraConsumoInterno { get; set; }
-
     public virtual DbSet<EstadosSolicitudesCI> EstadosSolicitudesCI { get; set; }
 
     public virtual DbSet<HistorialMovimientosSolicitudesCI> HistorialMovimientosSolicitudesCI { get; set; }
 
-    public virtual DbSet<LineasConsumoInterno> LineasConsumoInterno { get; set; }
+    public virtual DbSet<LineasConsumosInternos> LineasConsumosInternos { get; set; }
 
     public virtual DbSet<LineasSolicitudesCI> LineasSolicitudesCI { get; set; }
 
@@ -49,6 +48,29 @@ public partial class AppDataBase : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<CabeceraConsumosInternos>(entity =>
+        {
+            entity.HasKey(e => e.id_cabecera_consumo_interno).HasName("PK__Cabecera__79B996641EAD6DEE");
+
+            entity.HasOne(d => d.id_clasificacionNavigation).WithMany(p => p.CabeceraConsumosInternos)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__CabeceraS__id_cl__2EC5E7B8");
+
+            entity.HasOne(d => d.id_estado_solicitudNavigation).WithMany(p => p.CabeceraConsumosInternos)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__CabeceraS__id_es__2DD1C37F");
+
+            entity.HasOne(d => d.id_usuario_despachoNavigation).WithMany(p => p.CabeceraConsumosInternosid_usuario_despachoNavigation).HasConstraintName("FK__CabeceraS__id_us__30AE302A");
+
+            entity.HasOne(d => d.id_usuario_responsableNavigation).WithMany(p => p.CabeceraConsumosInternosid_usuario_responsableNavigation)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__CabeceraS__id_us__2FBA0BF1");
+
+            entity.HasOne(d => d.no_serie).WithMany(p => p.CabeceraConsumosInternos)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__CabeceraS__no_se__2CDD9F46");
+        });
+
         modelBuilder.Entity<CabeceraSolicitudesCI>(entity =>
         {
             entity.HasKey(e => e.id_cabecera_solicitud).HasName("PK_CabeceraSolicitudesActivas");
@@ -70,35 +92,11 @@ public partial class AppDataBase : DbContext
             entity.HasOne(d => d.no_serie).WithMany(p => p.CabeceraSolicitudesCI)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_CabeceraSolicitudes_NoSeries");
-        }
-        );
+        });
 
         modelBuilder.Entity<ClasificacionesCI>(entity =>
         {
             entity.HasKey(e => e.id_clasificacion).HasName("PK_ClasificacionesConsumoInterno");
-        });
-
-        modelBuilder.Entity<CabeceraSolicitudesCI>(entity =>
- {
-            entity.HasKey(e => e.id_cabecera_solicitud).HasName("PK_CabeceraSolicitudesActivas");
-
-            entity.HasOne(d => d.id_clasificacionNavigation).WithMany(p => p.CabeceraSolicitudesCI)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_CabeceraSolicitudes_Clasificaciones");
-
-            entity.HasOne(d => d.id_estado_solicitudNavigation).WithMany(p => p.CabeceraSolicitudesCI)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_CabeceraSolicitudes_EstadosSolicitudes");
-
-            entity.HasOne(d => d.id_usuario_despachoNavigation).WithMany(p => p.CabeceraSolicitudesCIid_usuario_despachoNavigation).HasConstraintName("FK__CabeceraS__id_us__6A1BB7B0");
-
-            entity.HasOne(d => d.id_usuario_responsableNavigation).WithMany(p => p.CabeceraSolicitudesCIid_usuario_responsableNavigation)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__CabeceraS__id_us__69279377");
-
-            entity.HasOne(d => d.no_serie).WithMany(p => p.CabeceraSolicitudesCI)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_CabeceraSolicitudes_NoSeries");
         });
 
         modelBuilder.Entity<EstadosSolicitudesCI>(entity =>
@@ -113,11 +111,11 @@ public partial class AppDataBase : DbContext
             entity.HasKey(e => e.id_hist_mov_solicitud).HasName("PK__Historia__D655A71D4DC2C063");
         });
 
-        modelBuilder.Entity<LineasConsumoInterno>(entity =>
+        modelBuilder.Entity<LineasConsumosInternos>(entity =>
         {
             entity.HasKey(e => e.id_linea_consumo_interno).HasName("PK__LineasSo__A0E9C7BCE52F5EAC");
 
-            entity.HasOne(d => d.cabecera_consumo_interno).WithMany(p => p.LineasConsumoInterno)
+            entity.HasOne(d => d.cabecera_consumo_interno).WithMany(p => p.LineasConsumosInternos)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK__LineasCon__cabec__3EFC4F81");
         });
@@ -148,21 +146,6 @@ public partial class AppDataBase : DbContext
             entity.HasOne(d => d.posicion).WithMany(p => p.UsuariosCI)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Usuarios_Posiciones");
-        });
-
-        modelBuilder.Entity<UsuarioPerfil>(entity =>
-        {
-            entity.HasOne(d => d.perfil).WithMany(p => p.usuarioPerfil).HasConstraintName("FK_UsuarioPerfil_Perfil");
-        });
-
-        modelBuilder.Entity<Perfil>(entity =>
-        {
-            entity.HasOne(d => d.aplicacion).WithMany(p => p.perfil).HasConstraintName("FK_Perfil_Aplicacion");
-        });
-
-        modelBuilder.Entity<NoSeries>(entity =>
-        {
-            entity.HasKey(e => e.id_no_serie).HasName("FP_NoSeries_PK");
         });
 
         OnModelCreatingPartial(modelBuilder);
