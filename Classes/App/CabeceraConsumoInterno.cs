70bcebca20daba58d5c31ed6b549b8b89dd3b686
Edilson Alcantara <edilsona@bellon.com.do>
2025-02-20 08:30:18 -0400

    agregando los modelos o DTOs de consumos internos

---
+++ Classes/App/CabeceraConsumoInterno.cs
@@ -0,0 +1,65 @@
+// Root myDeserializedClass = JsonSerializer.Deserialize<List<Root>>(myJsonResponse);
+using System.Text.Json.Serialization;
+using Bellon.API.ConsumoInterno.DataBase;
+
+namespace Bellon.API.ConsumoInterno.Classes;
+
+[Serializable]
+public class CabeceraConsumoInterno
+{
+
+    [JsonPropertyName("id_cabecera_consumo_interno")]
+    public int? IdCabeceraConsumoInterno { get; set; }
+
+    [JsonPropertyName("no_serie_id")]
+    public int? NoSerieId { get; set; }
+
+    [JsonPropertyName("no_documento")]
+    public string? NoDocumento { get; set; }
+
+    [JsonPropertyName("fecha_creado")]
+    public DateTime FechaCreado { get; set; }
+
+    [JsonPropertyName("creado_por")]
+    public string CreadoPor { get; set; }
+
+    [JsonPropertyName("usuario_responsable")]
+    public string UsuarioResponsable { get; set; }
+
+    [JsonPropertyName("usuario_despacho")]
+    public string UsuarioDespacho { get; set; }
+
+    [JsonPropertyName("id_departamento")]
+    public string IdDepartamento { get; set; }
+
+    [JsonPropertyName("id_estado_solicitud")]
+    public int IdEstadoSolicitud { get; set; }
+
+    [JsonPropertyName("id_clasificacion")]
+    public int IdClasificacion { get; set; }
+
+    [JsonPropertyName("id_sucursal")]
+    public string IdSucursal { get; set; }
+
+    [JsonPropertyName("fecha_modificado")]
+    public DateTime? FechaModificado { get; set; }
+
+    [JsonPropertyName("modificado_por")]
+    public string ModificadoPor { get; set; }
+
+    [JsonPropertyName("comentario")]
+    public string Comentario { get; set; }
+
+    [JsonPropertyName("total")]
+    public decimal Total { get; set; }
+
+    [JsonPropertyName("id_usuario_responsable")]
+    public string IdUsuarioResponsable { get; set; }
+
+    [JsonPropertyName("id_usuario_despacho")]
+    public string IdUsuarioDespacho { get; set; }
+
+    [JsonPropertyName("LineasConsumoInterno")]
+    public List<LineasConsumoInterno> Lineas { get; set; }
+
+}
---
+++ Classes/App/LineasConsumoInterno.cs
@@ -0,0 +1,46 @@
+// Root myDeserializedClass = JsonSerializer.Deserialize<List<Root>>(myJsonResponse);
+using System.Text.Json.Serialization;
+
+namespace Bellon.API.ConsumoInterno.Classes;
+
+[Serializable]
+public class LineasConsumoInterno
+{
+
+    [JsonPropertyName("id_linea_consumo_interno")]
+    public int? IdLineaConsumoInterno { get; set; }
+
+    [JsonPropertyName("cabecera_consumo_interno_id")]
+    public int CabeceraConsumoInternoId { get; set; }
+
+    [JsonPropertyName("id_producto")]
+    public string IdProducto { get; set; }
+
+    [JsonPropertyName("no_producto")]
+    public string NoProducto { get; set; }
+
+    [JsonPropertyName("descripcion")]
+    public string Descripcion { get; set; }
+
+    [JsonPropertyName("precio_unitario")]
+    public decimal PrecioUnitario { get; set; }
+
+    [JsonPropertyName("cantidad")]
+    public int Cantidad { get; set; }
+
+    [JsonPropertyName("id_unidad_medida")]
+    public string IdUnidadMedida { get; set; }
+
+    [JsonPropertyName("codigo_unidad_medida")]
+    public string CodigoUnidadMedida { get; set; }
+
+    [JsonPropertyName("almacen_id")]
+    public string? AlmacenId { get; set; }
+
+    [JsonPropertyName("almacen_codigo")]
+    public string? AlmacenCodigo { get; set; }
+
+    [JsonPropertyName("nota")]
+    public string? Nota { get; set; }
+
+}
