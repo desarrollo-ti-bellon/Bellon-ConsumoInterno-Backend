a8b2c8036d86e82b04518e247503845fbcd3acb0
Edilson Alcantara <edilsona@bellon.com.do>
2025-03-07 17:05:48 -0400

    creando un servicio en segundo plano para llamar el servicio de productos y obtener el listado de productos automaticamente.

---
+++ Services/ServicioSegundoPlano.cs
@@ -0,0 +1,37 @@
+using Bellon.API.ConsumoInterno.Interfaces;
+
+public class ServicioSegundoPlano : BackgroundService
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public ServicioSegundoPlano(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            // Crear un scope de servicios para poder resolver servicios scoped
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var _servicioProducto = scope.ServiceProvider.GetRequiredService<IServicioProducto>();
+
+                try
+                {
+                    // Ejecutar la lógica del servicio scoped
+                    await _servicioProducto.ObtenerProductos();
+                }
+                catch (Exception ex)
+                {
+                    // Manejo de excepciones
+                    Console.WriteLine($"Error al obtener productos: {ex.Message}");
+                }
+            }
+
+            // Esperar 5 segundos entre ejecuciones
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+}
