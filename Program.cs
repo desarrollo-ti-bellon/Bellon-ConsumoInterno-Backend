713dfa5336324c9763a65246dbf0ba4b14586f01
Edilson Alcantara <edilsona@bellon.com.do>
2025-04-02 10:47:35 -0400

    indentando codigo

--- Program.cs
+++ Program.cs
@@ -146,10 +146,7 @@ builder.Services.AddScoped<IServicioProveedor, ServicioProveedor>();
 builder.Services.AddScoped<IServicioUnidadMedida, ServicioUnidadMedida>();
 builder.Services.AddScoped<IServicioSolicitud, ServicioSolicitud>();
 builder.Services.AddScoped<IServicioConsumoInterno, ServicioConsumoInterno>();
-builder.Services.AddScoped<
-    IServicioHistorialMovimientosSolicitudes,
-    ServicioHistoricoMovimientoSolicitud
->();
+builder.Services.AddScoped<IServicioHistorialMovimientosSolicitudes,ServicioHistoricoMovimientoSolicitud>();
 builder.Services.AddScoped<IServicioEstadoSolicitud, ServicioEstadoSolicitud>();
 builder.Services.AddScoped<IServicioNotas, ServicioNotas>();
 builder.Services.AddScoped<IServicioClasificacion, ServicioClasificacion>();
